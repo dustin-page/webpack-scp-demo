30689ba40c5e5be25e110b367e3d06a8b286cbe0
Page, Dustin <dustin.page@mckesson.com>
2019-04-22 12:15:23 -0500

    Integrate React into app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('Webpack 4');
+
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+class App extends Component {
+    constructor( props ) {
+        super( props );
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello React!</h1>
+            </div>
+        );
+    }
+}
+
+// render inside `app-root` element
+ReactDOM.render( <App />, document.getElementById( 'app-root' ) );
